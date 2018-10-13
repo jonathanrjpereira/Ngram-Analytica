d4e8591362056c8ba991a534f3b5d744cd4af52b
Kanishk Singh <singh.kanishk07@gmail.com>
2018-10-13 17:08:14 +0530

    Refactored code and added date range, smoothing

--- ngram.py
+++ ngram.py
@@ -1,27 +1,21 @@
-
+import sys
 import requests
 import re
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
 
-ngram = "farrago" #Example Word: farrago
+def graph(content, start, end, smoothing):
+    url = "https://books.google.com/ngrams/graph"
 
-def graph(word):
+    querystring = {"content":content,
+                   "case_insensitive":"on",
+                   "year_start":start,
+                   "year_end":end,
+                   "corpus":"15",
+                   "smoothing":smoothing}
 
-    #page = requests.get("https://books.google.com/ngrams/graph?content=farrago&year_start=1800&year_end=2008&corpus=15&smoothing=8&share=&direct_url=t1%3B%2Cfarrago%3B%2Cc0")
-
-
-    url1 = "https://books.google.com/ngrams/graph?content="
-    url2 = "&year_start=1800&year_end=2008&corpus=15&smoothing=8&share=&direct_url=t1%3B%2C"
-    url3 = "%3B%2Cc0"
-    url = url1 + str(word) + url2 + str(word) + url3
-    #print (url)
-
-    page = requests.get(url)
-    #print (page.status_code)
-
-    #print (page.content)
-
-    from bs4 import BeautifulSoup
+    page = requests.request("GET", url, params=querystring)
+    
     soup = BeautifulSoup(page.content, 'html.parser')
 
     data = []
@@ -33,30 +27,29 @@ def graph(word):
     try:
         found = re.search('"timeseries": (.+?)], "pare', graph).group(1)    #Start & End of graph data
     except AttributeError:
-        found = 'Error' # apply your error handling
-    #print (found)
+        print("Encountered an error! Here's the response from the page\n Response: {}".format(page.content)) # apply your error handling
+        sys.exit() # Exit gracefully
 
-    list = found.split (', ')   #Convert graph's data points into list
-    list = [s.replace('[', '') for s in list]   #Removes any garbage values from data.
-    #print (len(list))
+    datapoints = found.split (', ')   #Convert graph's data points into list
+    datapoints = [datapoint.replace('[', '') for datapoint in datapoints]   #Removes any garbage values from data.
 
 
-    list = [float(s) for s in list]
+    datapoints = [float(datapoint) for datapoint in datapoints]
     #list = [float(s)*10000000 for s in list]    #Coverts data points from floating exponential values to float
     # 10,000,000 is used such that numbers with value 10e-5 are less than 1
 
-    #print (len(list))
-    print (list)
-    return list
+    return datapoints
 
+def ngram_viewer(content, start, end, smoothing):
+    graph_plot = graph(content, start, end, smoothing)
+    year = list(range(start, end))  # X-axis Co-ordinates
 
-graph_plot = graph(ngram)
+    plt.plot(year, graph_plot)  #Plot X=Year , Y=Frequency(%)
+    plt.axis([start, end, min(graph_plot), max(graph_plot)])    # X & Y axis Range
+    plt.xlabel('Year')
+    plt.ylabel('Frequency(%)')
+    plt.show()
 
-year = list(range(1800, 2009))  # X-axis Co-ordinates
-#print (year)
-
-plt.plot(year, graph_plot)  #Plot X=Year , Y=Frequency(%)
-plt.axis([1800, 2008, min(graph_plot), max(graph_plot)])    # X & Y axis Range
-plt.xlabel('Year')
-plt.ylabel('Frequency(%)')
-plt.show()
+if __name__ == '__main__':
+    content = "Farrago"
+    ngram_viewer(content, 1800, 2009, smoothing=3)
